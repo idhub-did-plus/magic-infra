4abcca88b2e3a21d040cf49510ac0e19837fed07
baiyuqi <1353813001@qq.com>
2019-11-07 14:53:12 +0800

    dd

--- main.go
+++ main.go
@@ -2,12 +2,17 @@
 package main
 
 import (
-	_ "fmt"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
-	"net/http"
-
 	"magic-infra/model"
 	"magic-infra/service"
+	"net/http"
+	"strings"
+
+	"encoding/json"
 
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -42,11 +47,44 @@ func listDeployedTokens() *[]model.DeployedToken {
 	return &result
 
 }
-func myHandler(w http.ResponseWriter, r *http.Request) {
+func saveDeployedToken(w http.ResponseWriter, r *http.Request) {
+	jsonb, _ := ioutil.ReadAll(r.Body)
+	jsons := bytes.NewReader(jsonb)
+	dec := json.NewDecoder(jsons)
+	result := model.DeployedToken{}
+	dec.Decode(&result)
 
+	session := service.Session()
+
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("InfraRepository").C("DeployedToken")
+	c.Insert(&result)
 }
 
 func main() {
-	http.HandleFunc("/", myHandler) //	设置访问路由
+	http.HandleFunc("/saveDeployedToken", saveDeployedToken) //	设置访问路由
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+func test() {
+	const jsonStream = `
+	{"Name": "Ed", "Text": "Knock knock."}
+	{"Name": "Sam", "Text": "Who's there?"}
+	{"Name": "Ed", "Text": "Go fmt."}
+	{"Name": "Sam", "Text": "Go fmt who?"}
+	{"Name": "Ed", "Text": "Go fmt yourself!"}
+`
+	type Message struct {
+		Name, Text string
+	}
+	dec := json.NewDecoder(strings.NewReader(jsonStream))
+	for {
+		var m Message
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s: %s\n", m.Name, m.Text)
+	}
 }
