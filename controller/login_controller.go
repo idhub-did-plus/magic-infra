cfcb261359903a06be182299c05982157354118e
baiyuqi <yuqi.bai8888@gmail.com>
2019-12-03 06:17:54 +0800

    ddd

--- controller/login_controller.go
+++ controller/login_controller.go
@@ -2,13 +2,34 @@
 package controller
 
 import (
+	"magic-infra/middleware"
 	"net/http"
+
+	"github.com/google/uuid"
+
+	"fmt"
+	"log"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func Login(c *gin.Context) {
+	username, _ := c.PostForm("username"), c.PostForm("password")
+	// Authentication
+	// blahblah...
+
+	// Generate random session id
+	u, err := uuid.NewRandom()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sessionId := fmt.Sprintf("%s-%s", u.String(), username)
+	// Store current subject in cache
+	middleware.GlobalCache.Set(sessionId, []byte(username))
+	// Send cache key back to client in cookie
+	c.SetCookie("current_subject", sessionId, 30*60, "/resource", "", false, true)
+	//c.JSON(200, component.RestResponse{Code: 1, Message: username + " logged in successfully"})
 	session := sessions.Default(c)
 	if session.Get("hello") != "world" {
 		session.Set("hello", "world")
