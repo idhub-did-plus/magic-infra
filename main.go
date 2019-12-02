83a50bed3392d6a1529365799b26df0d9ecbaa4a
baiyuqi <1353813001@qq.com>
2019-12-02 15:30:30 +0800

    kk

--- main.go
+++ main.go
@@ -104,33 +104,18 @@ func main() {
 	r.Use(CorsMiddleware())
 
 	store := cookie.NewStore([]byte("secret11111"))
-	// 设置session中间件，参数mysession，指的是session的名字，也是cookie的名字
-	// store是前面创建的存储引擎，我们可以替换成其他存储引擎
 	r.Use(sessions.Sessions("mysession", store))
 	r.POST("/saveDeployedToken", saveDeployedToken)
 	r.GET("/listDeployedTokens", listDeployedTokens)
 	r.GET("/hello", func(c *gin.Context) {
-		// 初始化session对象
 		session := sessions.Default(c)
-
-		// 通过session.Get读取session值
-		// session是键值对格式数据，因此需要通过key查询数据
 		if session.Get("hello") != "world" {
-			// 设置session数据
 			session.Set("hello", "world")
-			// 删除session数据
 			session.Delete("tizi365")
-			// 保存session数据
 			session.Save()
-			// 删除整个session
-			// session.Clear()
 		}
 
 		c.JSON(200, gin.H{"hello": session.Get("hello")})
 	})
 	r.Run() // 在 0.0.0.0:8080 上监听并服务
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/saveDeployedToken", saveDeployedToken) //	设置访问路由
-	// mux.HandleFunc("/listDeployedTokens", listDeployedTokens)
-	// log.Fatal(http.ListenAndServe(":8080", nil))
 }
