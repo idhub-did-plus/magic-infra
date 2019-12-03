5260900b4d53d611bedf4edf58f688919333062d
baiyuqi <1353813001@qq.com>
2019-12-03 10:14:19 +0800

    dd

---
+++ component/claim_registry.go
@@ -0,0 +1,40 @@
+// claim_registry
+package component
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+const abiraw = `[
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "param",
+				"type": "address"
+			}
+		],
+		"name": "addrTy",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]`
+
+func main() {
+	abi, err := abi.JSON(strings.NewReader(abiraw))
+	if err != nil {
+		panic(err)
+	}
+	address := common.HexToAddress("0xca35b7d915458ef540ade6068dfe2f44e8fa733c")
+	out, err := abi.Pack("addrTy", address)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%x\n", out[4:])
+}
