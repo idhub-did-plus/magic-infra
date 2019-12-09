a9fd0cdf9a6b25b526dc85614e0c97221620e172
baiyuqi <yuqi.bai8888@gmail.com>
2019-12-10 00:01:00 +0800

    dd

--- controller/token_controller.go
+++ controller/token_controller.go
@@ -45,7 +45,7 @@ func CorsMiddleware() gin.HandlerFunc {
 
 		method := c.Request.Method
 
-		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
