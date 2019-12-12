087821f87f4c8eec24fb98622c627c54c941c4ec
baiyuqi <1353813001@qq.com>
2019-12-12 15:39:57 +0800

    dd

--- doc.go
+++ doc.go
@@ -7,3 +7,4 @@ package main
 
 //https://dev.to/maxwellhertz/tutorial-integrate-gin-with-cabsin-56m0
 //https://medium.com/alpineintel/issuing-and-verifying-eip-712-challenges-with-go-32635ca78aaf
+//https://kauri.io/article/60a36c1b17d645939f63415218dc24f9/creating-a-dapp-in-go-with-geth
