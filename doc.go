db105959e156e3377f0452e53c97ea2000040f7e
baiyuqi <1353813001@qq.com>
2019-12-13 14:58:25 +0800

    dd

--- doc.go
+++ doc.go
@@ -8,3 +8,4 @@ package main
 //https://dev.to/maxwellhertz/tutorial-integrate-gin-with-cabsin-56m0
 //https://medium.com/alpineintel/issuing-and-verifying-eip-712-challenges-with-go-32635ca78aaf
 //https://kauri.io/article/60a36c1b17d645939f63415218dc24f9/creating-a-dapp-in-go-with-geth
+//https://goethereumbook.org/smart-contract-load/
--- service/claim_service.go
+++ service/claim_service.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 
@@ -18,6 +19,14 @@ func main() {
 
 	address := common.HexToAddress("0x147B8eb97fD247D06C4006D269c90C1908Fb5D54")
 	instance, err := store.NewComponentCaller(address, client)
+	issuer := common.HexToAddress("0x147B8eb97fD247D06C4006D269c90C1908Fb5D54")
+	identity := common.HexToAddress("0x147B8eb97fD247D06C4006D269c90C1908Fb5D54")
+	var data []byte = []byte("valid investor")
+	array := common.BytesToHash(data).Bytes()
+	var array32 [32]byte
+	copy(array32[:], array)
+
+	instance.GetClaim(&bind.CallOpts{}, issuer, identity, array32)
 	if err != nil {
 		log.Fatal(err)
 	}
