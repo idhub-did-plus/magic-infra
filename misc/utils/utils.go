836b92b0020d01c08e4d1a9f383a66c0127e398b
baiyuqi <1353813001@qq.com>
2019-12-16 14:42:32 +0800

    dd

--- misc/utils/utils.go
+++ misc/utils/utils.go
@@ -2,10 +2,15 @@ package utils
 
 import (
 	"github.com/ethereum/go-ethereum/common"
+
+     solsha3 "github.com/miguelmota/go-solidity-sha3"
 )
 
 func Sha3(value string) [32]byte {
 	var data []byte = []byte(value)
+	  hash := solsha3.SoliditySHA3(
+        solsha3.Bytes32(value)
+    )
 	array := common.BytesToHash(data).Bytes()
 	var result [32]byte
 	copy(result[:], array)
