2cb3c5a5f5a21eee918214292afe911917792eb2
baiyuqi <1353813001@qq.com>
2019-12-20 10:46:12 +0800

    dd

--- service/datastore.go
+++ service/datastore.go
@@ -1,12 +1,21 @@
 package service
 
 import (
+	"fmt"
 	"magic-infra/config"
 
 	"gopkg.in/mgo.v2"
 )
 
-var mysession, err = mgo.Dial(config.Config.Mysql.Url)
+var mysession *mgo.Session
+
+func init() {
+	var err error
+	mysession, err = mgo.Dial(config.Config.Mysql.Url)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 func Session() *mgo.Session {
 	return mysession.Copy()
