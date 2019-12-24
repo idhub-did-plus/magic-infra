4e6fe60f8099c51bc08e4157cca3b8f7f8627d2a
baiyuqi <yuqi.bai8888@gmail.com>
2019-12-25 07:35:32 +0800

    dd

--- doc.go
+++ doc.go
@@ -10,3 +10,5 @@ package main
 //https://kauri.io/article/60a36c1b17d645939f63415218dc24f9/creating-a-dapp-in-go-with-geth
 //https://goethereumbook.org/smart-contract-load/
 //https://www.alpineintel.com/issuing-and-verifying-eip-712-challenges-with-go/
+//https://www.tizi365.com/archives/340.html
+//https://books.studygolang.com/gopl-zh/ch2/ch2-06.html
